957d969ebfbc79a526b9122f9264538f4425d922
jesuscb123 <cbjesus@iesrafaelalberti.es>
2025-11-10 12:17:09 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ej2_Tema3_JesusCondeBarba',
+  webDir: 'www'
+};
+
+export default config;
